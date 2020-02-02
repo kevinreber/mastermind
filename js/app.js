c1b93f77063b1ab17f4161d35829bb828514bb12
kevinreber <kevinreber1@gmail.com>
2020-02-01 17:54:51 -0800

    Added number comparison

--- js/app.js
+++ js/app.js
@@ -1,30 +1,12 @@
-const randomAPI = 'https://www.random.org/integers/?num=4&min=0&max=8&col=1&base=16&format=plain';
+const randomAPI = 'https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=16&format=plain';
 const keyboard = document.getElementById('keyboard');
 const randomNumbers = document.getElementById('random-numbers');
+const usersGuesses = document.getElementById('users-guesses');
+const usersGuess = usersGuesses.querySelectorAll('.user-guess');
+const attempts = document.getElementById('attempts');
+let attemptsLeft = 10;
 let randomAPIResults = [];
 let userInput = []; //store user input(similar to Simon Game)
-
-// fetch(randomAPI)
-//     .then(checkStatus)
-//     //.then(res => res.json())
-//     .then(storeData)
-//     .catch(error => console.log('Looks like there was a problem', error));
-
-// function checkStatus(response) {
-//     if (response.ok) { //check if status range 200-299
-//         return Promise.resolve(response);
-//     } else {
-//         return Promise.reject(new Error(response.statusText));
-//     }
-// }
-
-// function storeData(res){
-//     const numList = randomAPI.responseText;
-//     console.log(res);
-
-// randomAPIResults = [...res];
-// return randomAPIResults;
-//}
 
 // SOURCE
 // https://stackoverflow.com/questions/6375461/get-html-code-using-javascript-with-a-url
@@ -33,8 +15,9 @@ request.open("GET", randomAPI, true);
 request.send(null);
 request.onreadystatechange = function () {
     if (request.readyState === 4) {
-        let nums = request.responseText.replace(/\s+/g, '');    // remove carriage return
+        let nums = request.responseText.replace(/\s+/g, ''); // remove carriage return
         randomAPIResults = [...nums];
+        addRandomNumbers();
     }
 };
 
@@ -52,16 +35,11 @@ function makeHttpObject() {
     throw new Error("Could not create HTTP request object.");
 }
 
-
-//user needs to guess number and number location
-
-//
-
 function addRandomNumbers() {
     let html = '';
     for (let randomNumber of randomAPIResults) {
         html +=
-        `
+            `
         <div class="random-number-container container">
             <p class="random-number number">${randomNumber}</p>
         </div>
@@ -70,9 +48,101 @@ function addRandomNumbers() {
     randomNumbers.innerHTML = html;
 }
 
-// document.addEventListener("DOMContentLoaded", addRandomNumbers());
+function userSelection(e) {
+    userInput.push(e.target.innerText);
+    displayUsersGuess();
+    //compareNumber();
+    if (userInput.length === 4) {
+        //console.log(submitAttempt);
+        checkAnswers();
+        updateAttempts();
+    }
+}
+
+function updateAttempts() { // updates attempts made after 4 guesses have been made
+    attemptsLeft--;
+    attempts.innerText = attemptsLeft;
+}
+
+function checkAnswers() {
+    let html = '';
+
+    const correctNumbers = checkIfNumberExists(); //  The player had guess a correct number
+    const correctMatches = checkForMatches(); // The player had guessed a correct number and its correct location
+
+    if (correctMatches === 4) {
+        html = 'Congratulations! You Win!'
+    } else if (correctNumbers) {
+        html = `
+        Player has guessed ${correctNumbers}/4 numbers that exist,
+        Player has guessed ${correctMatches}/4 numbers in the correct location
+        `;
+    } else html = 'You have guessed wrong! Try Again';
+
+    console.log(html);
 
 
-keyboard.addEventListener('click', function (e) {
-    console.log(e.target.innerText);
-})
+
+
+}
+
+function checkIfNumberExists() {
+    let exists = 0;
+    for (let num of randomAPIResults) {
+        if (userInput.includes(num)) {
+            exists++;
+        }
+    }
+    return exists;
+
+    // if (randomAPIResults.some(number => userInput.includes(number))) {
+    //     console.log('some');
+    //}
+}
+
+function checkForMatches() {
+    let matches = 0;
+    for (let i = 0; i < randomAPIResults.length; i++) {
+        if (randomAPIResults[i] === userInput[i]) {
+            matches++;
+        }
+    }
+    return matches;
+}
+
+// function displayUsersGuess(){
+//     let html = '';
+//     for (let usersGuess of userInput) {
+//         html +=
+//         `
+//         <div class="users-number-container container">
+//             <p class="user-number number">${usersGuess}</p>
+//         </div>
+//         `;
+//     }
+//     usersGuesses.innerHTML = html;
+// }
+
+function displayUsersGuess() {
+    for (let [index, guess] of usersGuess.entries()) {
+        if (!userInput[index]) return;
+        // console.log(guess,index);
+        guess.innerText = userInput[index];
+    }
+}
+
+function compareNumber() {
+    console.log(userInput);
+    console.log(userInput.length);
+
+    for (let i = 0; i < userInput.length; i++) {
+
+        if (userInput[i] === randomAPIResults[i]) {
+            console.log('match');
+
+        } else console.log('no match');
+
+    }
+}
+
+keyboard.addEventListener('click', userSelection);
