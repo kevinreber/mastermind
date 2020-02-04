b7030ec94cecd73b41bd171083499b3cdc2657c6
kevinreber <kevinreber1@gmail.com>
2020-02-03 19:55:07 -0800

    updated XMLHttpRequest

--- js/app.js
+++ js/app.js
@@ -1,43 +1,37 @@
-const randomAPI = 'https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=16&format=plain';
+const query = '?num=4&min=0&max=7&col=1&base=16&format=plain';
+const url = `https://www.random.org/integers/${query}`;
 const keyboard = document.getElementById('keyboard');
 const randomNumbers = document.getElementById('random-numbers');
-const usersGuesses = document.getElementById('users-guesses');
-const usersGuess = usersGuesses.querySelectorAll('.user-guess');
-const attempts = document.getElementById('attempts');
+//const usersGuesses = document.getElementById('users-guesses');
+const userGuess = document.querySelectorAll('.user-guess');
+const attemptsLeft = document.getElementById('attempts-left');
+//const attempts = document.querySelectorAll('.attempt');
 const tableAttempts = document.querySelectorAll('.table-attempt');
-let attemptsLeft = 10;
-let attempt = 0;
-let randomAPIResults = [];
-let userInput = []; //store user input(similar to Simon Game)
+const firstReq = new XMLHttpRequest();
+let attemptsUserHasLeft = 10;
+let attemptUserIsOn = 0; // Reference to update user history
+let randomAPIResults = []; // Store numbers from API call
+let userInput = []; // store user input
 
 // SOURCE
 // https://stackoverflow.com/questions/6375461/get-html-code-using-javascript-with-a-url
-const request = makeHttpObject();
-request.open("GET", randomAPI, true);
-request.send(null);
-request.onreadystatechange = function () {
-    if (request.readyState === 4) {
-        let nums = request.responseText.replace(/\s+/g, ''); // remove carriage return
-        randomAPIResults = [...nums];
-        addRandomNumbers();
-    }
-};
 
-function makeHttpObject() {
-    try {
-        return new XMLHttpRequest();
-    } catch (error) {}
-    try {
-        return new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (error) {}
-    try {
-        return new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (error) {}
+firstReq.addEventListener('load', () => console.log('SUCCESS!'));
+firstReq.addEventListener('error', () => console.log('ERROR'));
 
-    throw new Error("Could not create HTTP request object.");
+function startGame() {
+    firstReq.open('GET', url); // Make API call
+    firstReq.send();
+    firstReq.onreadystatechange = function () {
+        if (firstReq.readyState === 4) {
+            const response = firstReq.responseText.replace(/\s+/g, ''); // remove carriage return            
+            randomAPIResults = [...response];
+            displayRandomNumbers();
+        }
+    };
 }
 
-function addRandomNumbers() {
+function displayRandomNumbers() {
     let html = '';
     for (let randomNumber of randomAPIResults) {
         html +=
@@ -50,53 +44,38 @@ function addRandomNumbers() {
     randomNumbers.innerHTML = html;
 }
 
-function userSelection(e) {
-    userInput.push(e.target.innerText);
-    displayUsersGuess();
+function userMakesGuess(e) {
+    const key = e.target;
+    if (key.tagName === 'BUTTON') {
+        userInput.push(key.innerText);
+        displayGuessMade();
+    }
     if (userInput.length === 4) {
         checkAnswers();
         updateAttempts();
     }
 }
 
-function updateAttempts() { // updates attempts made after 4 guesses have been made
-    attemptsLeft--;
-    attempts.innerText = attemptsLeft;
+function displayGuessMade() {
+    for (let [index, guess] of userGuess.entries()) {
+        if (!userInput[index]) return;
+        guess.innerText = userInput[index];
+    }
 }
 
 function checkAnswers() {
-    const correctNumbers = checkIfNumberExists(); //  The player had guess a correct number
+    const correctNumbers = checkIfNumberExists(); // The player had guess a correct number
     const correctMatches = checkForMatches(); // The player had guessed a correct number and its correct location
 
     displayResults(correctNumbers, correctMatches);
     updateHistory(correctNumbers, correctMatches);
+
     clearUserGuesses();
 }
 
-function displayResults(correctNumbers, correctMatches) {
-    let html = '';
-    if (correctMatches === 4) {
-        html = 'Congratulations! You Win!'
-    } else if (correctNumbers) {
-        html = `
-        Player has guessed ${correctNumbers}/4 numbers that exist,
-        Player has guessed ${correctMatches}/4 numbers in the correct location
-        `;
-    } else html = 'You have guessed wrong! Try Again';  
-    console.log(html);
-}
-
-function updateHistory(correct, located) {
-    let html = `
-        <td class="attempt">${userInput}</td>
-        <td class="attempt">${correct}/4</td>
-        <td class="attempt">${located}/4</td>
-    `
-    tableAttempts[attempt].innerHTML = html;
-    attempt++;
-    if(attempt === 10 || attemptsLeft === 0){
-        console.log('failed');      
-    }
+function updateAttempts() { // updates attempts left after 4 guesses have been made
+    attemptsUserHasLeft--;
+    attemptsLeft.innerText = attemptsUserHasLeft;
 }
 
 function checkIfNumberExists() {
@@ -107,10 +86,6 @@ function checkIfNumberExists() {
         }
     }
     return exists;
-
-    // if (randomAPIResults.some(number => userInput.includes(number))) {
-    //     console.log('some');
-    //}
 }
 
 function checkForMatches() {
@@ -123,18 +98,54 @@ function checkForMatches() {
     return matches;
 }
 
-function displayUsersGuess() {
-    for (let [index, guess] of usersGuess.entries()) {
-        if (!userInput[index]) return;
-        guess.innerText = userInput[index];
+function displayResults(correctNumbers, correctMatches) {
+    let html = '';
+    if (correctMatches === 4) {
+        html = 'Congratulations! You Win!'
+    } else if (correctNumbers) {
+        html = `
+        Player has guessed ${correctNumbers}/4 numbers that exist,
+        Player has guessed ${correctMatches}/4 numbers in the correct location
+        `;
+    } else html = 'You have guessed wrong! Try Again';
+    console.log(html);
+}
+
+function updateHistory(correct, located) {
+    let html = `
+        <td class="attempt">${userInput}</td>
+        <td class="attempt">${correct}/4</td>
+        <td class="attempt">${located}/4</td>
+    `
+    tableAttempts[attemptUserIsOn].innerHTML = html;
+    attemptUserIsOn++;
+    if (attemptUserIsOn === 10 || attemptsUserHasLeft === 0) {
+        resetGame();
     }
 }
 
 function clearUserGuesses() {
-    for (let guess of usersGuess) {
+    for (let guess of userGuess) {
         guess.innerText = '-';
     }
     userInput = [];
 }
 
-keyboard.addEventListener('click', userSelection);
+function clearUserHistory() {
+    const attempts = document.querySelectorAll('.attempt');
+    for (let attempt of attempts) {
+        attempt.innerText = '-';
+    }
+}
+
+function resetGame() {
+    clearUserHistory();
+    clearUserGuesses();
+    attemptsUserHasLeft = 11; // subtracts 1 when game restarts
+    attemptUserIsOn = 0;
+    randomAPIResults = []; // clear API Results 
+    startGame();
+}
+
+document.addEventListener('DOMContentLoaded', startGame);
+keyboard.addEventListener('click', userMakesGuess);
